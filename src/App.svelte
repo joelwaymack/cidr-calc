e16b6294230df89b51e7b2b028471cfe1d661a5a
Joel Waymack <joel.waymack@microsoft.com>
2021-12-11 21:55:43 -0600

    Switch to typescript.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Octet from './components/Octet.svelte';
 
 	let cidr = 	{
---
+++ src/components/CidrInput.svelte
@@ -0,0 +1,11 @@
+<!-- <div class="ip">
+	<input type="number" name="octet1" bind:value={cidr.octet1} on:input={inputChange} />
+	<div class="dot">.</div>
+	<input type="number" name="octet2" bind:value={cidr.octet2} on:input={inputChange} />
+	<div class="dot">.</div>
+	<input type="number" name="octet3" bind:value={cidr.octet3} on:input={inputChange} />
+	<div class="dot">.</div>
+	<input type="number" name="octet4" bind:value={cidr.octet4} on:input={inputChange} />
+	<div class="dot">/</div>
+	<input type="number" name="mask" bind:value={cidr.mask} on:input={inputChange} />
+</div> -->
